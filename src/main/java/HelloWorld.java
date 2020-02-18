0250189071f5fc8095f4cd382322712994ad92bb
XodusDR <sadokun113@gmail.com>
2020-02-19 01:21:41 +0200

    MavenProjects Version 1

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String [] args)
+    {
+        System.out.println("Hello Bradley and Sherwin. Please edit my program and add your own text methods");
+
+    }
+}
